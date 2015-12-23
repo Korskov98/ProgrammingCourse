12887a902d3f7ee6fa1cdfd35aa8e4841a5224ba
user <user@debian>
2015-12-24 01:47:26 +0300

    cppcheck

--- sources/home-work/lib/removal.c
+++ sources/home-work/lib/removal.c
@@ -2,9 +2,9 @@
 #include"removal.h"
 
 int removal(int number){
-    int numeral, result = 0, digit = 1;
+    int result = 0, digit = 1;
     while(number > 0){
-        numeral = number % 10;
+        int numeral = number % 10;
         number = number / 10;
         if (numeral % 2 == 0){
             result = result + digit * numeral;
--- sources/home-work/lib/removing_words.c
+++ sources/home-work/lib/removing_words.c
@@ -7,21 +7,20 @@
 void removing_words(char string[]){
     FILE * oFile;
     oFile = fopen("output_removing_words", "w");
-    int n,i = 0,j,l,k,flag;
+    int n,i = 0;
     n = strlen(string);
-    i = 0;
     char word[100];
     while(i <= n){
-        l=0;
-        while ((string[i] != ' ') && (i < n)){
+        int l=0;
+        while ((i < n) && (string[i] != ' ')){
             word[l] = string[i];
             i++;
             l++;
         }
         word[l]='\0';
-        j = i + 1;
-        k = 0;
-        flag = 1;
+        int j = i + 1;
+        int k = 0;
+        int flag = 1;
         for (j=i+1;j <= i + l; j++){
             if (word[k] != string[j]){
                 flag = 0;
